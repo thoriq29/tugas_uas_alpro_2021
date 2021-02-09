c440a3cf84acf1d98edf822c5239d486cfd5d3ec
thoriq29 <oiq2908@gmail.com>
2021-02-09 22:22:51 +0700

    update settings

--- food_catalog_client/settings.py
+++ food_catalog_client/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'food_catalog_client.urls'
@@ -130,3 +131,4 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'food_catalog_client/staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
